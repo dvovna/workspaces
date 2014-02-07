0b55151e3fd7b9f3c053baeade252b3e55ea1128
dvovna <d.vovna.dv@gmail.com>
2014-02-07 15:37:20 +0300

    structure;

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "gfx"
+javascripts_dir = "js"
+fonts_dir = "app/fonts"
+
+output_style = :nested
+environment = :development
+
+relative_assets = true
