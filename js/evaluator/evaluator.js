41785901752f2b437a1797272fa55976d3f4bfe2
dvovna <d.vovna.dv@gmail.com>
2014-03-16 12:13:13 +0300

    test;

--- js/evaluator/evaluator.js
+++ js/evaluator/evaluator.js
@@ -7,7 +7,7 @@
             this.options = options || {};
 
             console.log('success');
-            //what it should do:
+            //what it should do:s
             /**
              * 1. should remember 'disabled' properties for calculating
              * 2. should restore 'disabled' prop to the disabled container and exclude them from calculating
