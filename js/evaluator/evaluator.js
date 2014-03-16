4b364a48a06aee628689f9db93ab75ee431e7034
dvovna <d.vovna.dv@gmail.com>
2014-03-16 11:23:48 +0300

    css;

--- js/evaluator/evaluator.js
+++ js/evaluator/evaluator.js
@@ -1,9 +1,18 @@
 (function (W, Backbone) {
     'use strict';
 
+    window.W = W || {};
     W.Evaluator = Backbone.Router.extend({
         initialize: function (options) {
             this.options = options || {};
+
+            console.log('success');
+            //what it should do:
+            /**
+             * 1. should remember 'disabled' properties for calculating
+             * 2. should restore 'disabled' prop to the disabled container and exclude them from calculating
+             * 3. should  x
+             */
         }
     });
-}(window, Backbone));
+}(s2wwindow, Backbone));
